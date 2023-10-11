bd7068d916146d9c23c0d47b7a4e651ed44e703c
suyalamritanshu <amritanshu.suyall2@gmail.com>
2023-10-12 02:13:10 +0530

    error

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,57 +10,67 @@ router.get('/', (req, res) => {
 
 router.post('/register', async (req, res) => {
     try {
-    const { name, email, password } = req.body;
-
-        const user = new User({ name, email, password });
-        const payload = {
-            user: {
-                id: user._id
-            }
-        }
-        
-        await user.save();
-        jwt.sign(payload, process.env.jwtUserSecret, (err, token) => {
-            if (err) throw err;
-            res.status(201).send({
-                success: true,
-                token: token,
-                user: user,
-                message: "User Created Successfully"
+        const { name, email, password } = req.body;
+        let user_exist = await User.findOne({ email: email });
+        if (user_exist) {
+            return res.status(400).json({
+                success: false,
+                msg: 'User already exists'
             });
-        });
+        }
+        else {
+            const user = new User({ name, email, password });
+            const payload = {
+                user: {
+                    id: user._id
+                }
+            }
+
+            await user.save();
+            jwt.sign(payload, process.env.jwtUserSecret, (err, token) => {
+                if (err) throw err;
+                res.status(201).send({
+                    success: true,
+                    token: token,
+                    user: user,
+                    message: "User Created Successfully"
+                });
+            });
+
+        }
+
     }
 
     catch (err) {
-        res.status(400).send({ error: err });
+       console.log(err)
     }
 
 });
 router.post('/login', async (req, res) => {
-   try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
 
-    if(!user){
-        throw new Error('Unable to login , invalid credentials');
+        if (!user) {
+            throw new Error('Unable to login , invalid credentials');
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            throw new Error('Unable to login , invalid credentials');
+        }
+
+        const token = jwt.sign({
+            _id: user._id.toString()
+        }, process.env.JWT_SECRET_KEY);
+
+        res.send({ user, token, message: "Logged in successfully" });
     }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if(!isMatch){
-        throw new Error('Unable to login , invalid credentials');
-    }
-
-    const token = jwt.sign({
-        _id: user._id.toString()
-    }, process.env.JWT_SECRET_KEY );
-
-    res.send({ user, token , message: "Logged in successfully"});
-   }
     catch (err) {
         res.status(400).send({ error: err });
     }
- });
+});
 
 // register a user
 // login a user
